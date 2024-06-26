bba3612f28a35623c88ad7b732179f0313b9debd
ValeriyVinn <valeriy.all@gmail.com>
2024-06-26 20:48:03 +0300

    Changed to router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-import  Loader  from './Loader/Loader'
-import NotFound from '../pages/NotFound/NotFound';
+import { NavLink, Route, Routes } from "react-router-dom";
 
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
-import SharedLayout from './SharedLayout/SharedLayout';
-
-const Home = lazy(() => import('../pages/Home/Home'));
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 export const App = () => {
   return (
-    <>
-    
-      <Suspense fallback={<Loader />}>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Домашняя</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dogs">Коллекция</NavLink>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="/" element={<div>Домашняя</div>}></Route>
+        <Route path="/dogs" element={<div>Коллекция</div>}></Route>
       </Routes>
-      </Suspense>
-    </>
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/react-router">
       <App />
     </BrowserRouter>
   </React.StrictMode>
